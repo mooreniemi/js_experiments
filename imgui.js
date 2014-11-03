ef3db872c65697822fe302f1508deef14a6c79ff
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-02 22:29:08 -0500

    kinda messy, maybe off-track, but bumbling through the js towards 20:16 in vid

--- imgui.js
+++ imgui.js
@@ -8,6 +8,7 @@ var uiContext = {
     },
     // types of user interaction that are active: mouse over
     active: function(ui_id) {
+        // every time something is "active" it does at least this
         return ui_id.el().append(' and so on');
     }
 };
@@ -26,7 +27,28 @@ var uiId = function() {
 var Widget = function(name) {
   this.name = name;
 };
+
 Widget.prototype = new uiId();
+
+// bool
+var doButton = function(ui_context,id,text){
+  this.context = ui_context,
+  this.init = function() { $(id).html('<button>'+ text + '</button>') },
+  this.text = text;
+
+  // compares our id vs the active id
+  // if inside set hot
+  // if is active
+    // if mouse up (action over)
+    // but still hot then true
+    // also set not active
+  // else if is hot
+    // mouse went down
+    // set active
+  // draw the button etc
+
+  this.init();
+};
 
 function update() {
 
@@ -35,3 +57,4 @@ function update() {
 module.exports.uiContext = uiContext;
 module.exports.uiId = uiId;
 module.exports.Widget = Widget;
+module.exports.doButton = doButton;
--- spec/imguiTest.js
+++ spec/imguiTest.js
@@ -1,3 +1,7 @@
+// http://jlongster.com/Removing-User-Interface-Complexity,-or-Why-React-is-Awesome
+// https://mollyrocket.com/861
+// https://gist.github.com/jlongster/11192270
+
 var jsdom = require("jsdom").jsdom;
 global.document = jsdom("<button id='foo'>hello world</button><div id='bar'>bye now</div>");
 global.window = document.parentWindow;
@@ -6,9 +10,6 @@ var boot = require('../boot.js'),
     // sut
     imgGui = require('../imgui.js'),
     $ = require('../node_modules/jquery/dist/jquery');
-
-// http://jlongster.com/Removing-User-Interface-Complexity,-or-Why-React-is-Awesome
-// https://mollyrocket.com/861
 
 describe("some examples of imgui programming in javascript", function() {
     describe("#ui_context, models the state of the users interaction", function() {
@@ -27,10 +28,17 @@ describe("some examples of imgui programming in javascript", function() {
             expect(ui_context.active(ui_id).text()).toEqual("hello world and so on");
         });
 
-        it("routing events via update to active", function(){
-          // i click an element and an event fires
-          // event is caught by the update router
-          // update router forwards to active
+        it("routing events via update to active", function() {
+            // i click an element and an event fires
+            // event is caught by the update router
+            // update router forwards to active
+        });
+        it("uses weird button pattern", function() {
+            var Widget = imgGui.Widget,
+                ui_id = new Widget('#foo');
+
+            new imgGui.doButton(ui_id, ui_id.name,'yes');
+            expect($('#foo').text()).toEqual('yes');
         });
     });
 });

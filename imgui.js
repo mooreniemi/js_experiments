b3e6e32fc8a0c358792ea136467b801eb217ebac
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-02 19:27:10 -0500

    widgets share the uiid type, uiContext manipulates the dom

--- imgui.js
+++ imgui.js
@@ -1,23 +1,32 @@
+$ = require('./node_modules/jquery/dist/jquery');
+
 var uiContext = {
-    // types of user interactiont hat are hot: mouse click
+    // types of user interaction that are hot: mouse click
     // hot
     hot: function(ui_id) {
-        return ui_id.el;
+        return ui_id.el();
     },
     // types of user interaction that are active: mouse over
     active: function(ui_id) {
-        return ui_id.el;
+        return ui_id.el().append(' and so on');
     }
 };
 
-var uiId = {
-    get parent() {
-        return "parent_of_selector"
+var uiId = function() {
+    this.parent = function() {
+        return $(':parent');
     },
-    get el() {
-        return "selector"
+    this.el = function() {
+      // selector of a modeled object
+      // would be abstracted to call like a #name method
+        return $('#foo');
     }
 };
+
+var Widget = function(name) {
+  this.name = name;
+};
+Widget.prototype = new uiId();
 
 function update() {
 
@@ -25,3 +34,4 @@ function update() {
 
 module.exports.uiContext = uiContext;
 module.exports.uiId = uiId;
+module.exports.Widget = Widget;
--- spec/imguiTest.js
+++ spec/imguiTest.js
@@ -1,5 +1,5 @@
 var jsdom = require("jsdom").jsdom;
-global.document = jsdom("<div id='foo'>hello world</div>");
+global.document = jsdom("<button id='foo'>hello world</button><div id='bar'>bye now</div>");
 global.window = document.parentWindow;
 
 var boot = require('../boot.js'),
@@ -7,22 +7,30 @@ var boot = require('../boot.js'),
     imgGui = require('../imgui.js'),
     $ = require('../node_modules/jquery/dist/jquery');
 
-console.log($('#foo').text())
-
 // http://jlongster.com/Removing-User-Interface-Complexity,-or-Why-React-is-Awesome
 // https://mollyrocket.com/861
 
 describe("some examples of imgui programming in javascript", function() {
     describe("#ui_context, models the state of the users interaction", function() {
         it("has a ui_id to pass around", function() {
-            var ui_id = imgGui.uiId;
-            expect(ui_id.el).toEqual("selector");
+            var Widget = imgGui.Widget,
+                ui_id = new Widget('name');
+
+            expect(ui_id.el().text()).toEqual("hello world");
         });
 
         it("by capturing the currently used widget", function() {
-            var ui_id = imgGui.uiId,
+            var Widget = imgGui.Widget,
+                ui_id = new Widget('name'),
                 ui_context = imgGui.uiContext;
-            expect(ui_context.active(ui_id)).toEqual("selector");
+
+            expect(ui_context.active(ui_id).text()).toEqual("hello world and so on");
+        });
+
+        it("routing events via update to active", function(){
+          // i click an element and an event fires
+          // event is caught by the update router
+          // update router forwards to active
         });
     });
 });

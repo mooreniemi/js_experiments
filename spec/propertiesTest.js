8231a725ae668d542830c38b3adb1b335cfb9d74
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-02-21 15:31:24 -0500

    sketching out property test

--- spec/propertiesTest.js
+++ spec/propertiesTest.js
@@ -1,19 +1,29 @@
 require('../boot.js');
 
-var prop = require('../src/properties.js');
+var prop = require('../src/properties.js'),
+		jsc = require('../node_modules/jsverify/lib/jsverify.js');
 
 // http://jsverify.github.io/
 // http://math.stackexchange.com/questions/160945/does-commutativity-imply-associativity
 
 describe("let's prove a binary operator is commutative but not associative", function() {
-    describe("define the operator myBinOp", function() {
-        it("multiplies pairwise then adds one", function() {
-            var x = 1, y = 2;
-            expect(prop.myBinOp(x,y)).toBe(3);
-        });
-        it("multiplies pairwise then adds one", function() {
-            var x = 2, y = 3;
-            expect(prop.myBinOp(x,y)).toBe(7);
-        });
-    });
+	describe("define the operator myBinOp", function() {
+		it("multiplies pairwise then adds one", function() {
+			var x = 1, y = 2;
+			expect(prop.myBinOp(x,y)).toBe(3);
+			var a = 2, b = 3;
+			expect(prop.myBinOp(a,b)).toBe(7);
+		});
+	});
+	describe("define the property", function() {
+		it("obeysCommutativity", function() {
+			// forall (f : bool -> bool) (b : bool), f (f (f b)) = f(b).
+			var obeysCommutativity =
+				jsc.forall("nat -> nat -> nat", "nat", "nat", function (f, a, b) {
+					return f(a, b) === f(b, a);
+				});
+
+			jsc.assert(obeysCommutativity);
+		});
+	});
 });

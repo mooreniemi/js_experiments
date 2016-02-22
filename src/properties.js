1d602eaac004aae8a4489365bbdd3eccd313ee88
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-02-21 21:57:59 -0500

    sourcing

--- src/properties.js
+++ src/properties.js
@@ -1,7 +1,9 @@
+// http://math.stackexchange.com/questions/160945/does-commutativity-imply-associativity
 function myBinOp(x, y) {
   return (x * y) + 1;
 }
 
+// http://math.stackexchange.com/questions/327422/are-all-algebraic-commutative-operations-always-associative
 function myBinOp2(a, b) {
 	return (a * b) - (a + b);
 }

2bd8881930bf1a50423cbadec808b1bd06eb276d
Alex Moore-Niemi <amooreniemi@zipcar.com>
2016-02-24 11:49:45 -0500

    questionable use of object.prototype

--- src/crdts.js
+++ src/crdts.js
@@ -1,6 +1,6 @@
 var ops = {
   value: function(value) {
-    if(value !== undefined && value.hasOwnProperty("value")) {
+    if(value !== undefined && value.isA("Z")) {
       return value.value;
     } else if(value == undefined) {
       return 0;
@@ -13,7 +13,7 @@ var ops = {
     return new Z(this.value + 1);
   },
   add: function(zz) {
-    if(zz !== undefined && zz.hasOwnProperty("value")) {
+    if(zz !== undefined && zz.isA("Z")) {
       return new Z(this.value + zz.value);
     } else {
       return new Z(this.value + zz);
@@ -21,7 +21,14 @@ var ops = {
   }
 };
 
+function isA(type) {
+  return this.hasOwnProperty("type") && this.type === type;
+}
+
+Object.prototype.isA = isA;
+
 function Z(value) {
+  this.type = "Z";
   this.value = ops.value(value);
   this.succ = ops.succ;
   this.add = ops.add;
